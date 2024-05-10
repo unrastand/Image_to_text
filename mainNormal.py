4434932736d8eae7c69c4a1cce1dad1364503deb
unrastand <kolawoleisrael2500@gmail.com>
2024-05-10 15:23:48 +0100

    Added Arduino linking using Serial

--- mainNormal.py
+++ mainNormal.py
@@ -4,6 +4,21 @@ import pytesseract
 # Set the path to Tesseract executable (change this according to your installation)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+
+#list of saved license plates
+
+allowed_plates = ["ABS-234","23S-DAF"]
+
+# Serial port configuration
+serial_port = '/dev/ttyUSB0'  # Update this with your Arduino Nano's serial port
+baud_rate = 9600
+ser = serial.Serial(serial_port, baud_rate, timeout=1)
+
+
+# Function to send command to Arduino Nano to control motors
+def send_command(command):
+    ser.write(command.encode())
+
 # Function to capture image from system camera
 def capture_image():
     # Initialize the camera
@@ -32,8 +47,25 @@ def recognize_text(image):
 
     return text
 
+
+# Function to receive data from Arduino Nano
+def receive_data():
+    if ser.in_waiting > 0:
+        data = ser.readline().decode().strip()
+        return data
+    else:
+        return None
+
+
+# Function to send data to Arduino Nano to control motors
+def send_data(data):
+    ser.write(data.encode())
+
+
+
 # Main function
 def main():
+
     # Capture image from camera
     image = capture_image()
 
@@ -42,11 +74,23 @@ def main():
         # Recognize text from the captured image
         text = recognize_text(image)
 
-        # Print the recognized text
+        #check to open and close the gate 
+        for a in allowed_plates:
+            if text == a:
+                send_data("open")
+            
+
+        # Print the recognized text 
+        # please confirm what it prints from camera and saved to allowed_plates list
         print("Recognized Text:")
         print(text)
     else:
         print("Error: Failed to capture image.")
 
 if __name__ == "__main__":
-    main()
+    while True:
+        # 1 means data from arduino to start process
+
+        if receive_data() == 1:
+            main()
+        
